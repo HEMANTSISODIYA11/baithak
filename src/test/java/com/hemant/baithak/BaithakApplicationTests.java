98219dba7b8fdf9bf73a2b84bb7170d6a034c414
Hemant Singh Sisodiya <hemant@Hemants-MacBook-Pro.local>
2026-01-08 07:51:14 +0530

    Initialized the baithak application

---
+++ src/test/java/com/hemant/baithak/BaithakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hemant.baithak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaithakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
