51e1b75b06b446a1bd9dc4d2285f596f9a6b9a2f
HEMANTSISODIYA11 <sisodiyahemant123@gmail.com>
2026-02-07 11:17:02 +0530

    added configuration over object mapper config

--- src/main/java/com/hemant/baithak/configuration/ObjectMapperConfig.java
+++ src/main/java/com/hemant/baithak/configuration/ObjectMapperConfig.java
@@ -2,8 +2,10 @@ package com.hemant.baithak.configuration;
 
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import tools.jackson.databind.ObjectMapper;
 
+@Configuration
 public class ObjectMapperConfig {
 
   @Bean
